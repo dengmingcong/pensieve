d3350bb30eabdd14ee1912676deb144aa4e2e5f5
dengmingcong <mc.deng@outlook.com>
2018-10-14 15:52:53 +0800

    archive by year and tag

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
+from django.views.generic.dates import ArchiveIndexView
+
 from . import views
+from .models import Blog
 
 
 urlpatterns = [
+    # Index (home page)
     path('', views.Index.as_view(), name='index'),
-    path('blogs/', views.BlogListView.as_view(), name='blogs'),
+    # Archive
+    path('archive/', views.BlogArchiveView.as_view(), name='archive'),
+    # Archive by tag
+    path('archive/<str:tag>/', views.BlogArchivedByTagView.as_view(), name='archived-by-tag'),
+    # Blog detail specified by date and slug
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.BlogDetailView.as_view(), name='blog-detail'),
+    # Deprecated
     path('blog-author/<int:pk>', views.BlogAuthorDetailView.as_view(), name='blog-author-detail'),
+    # Page introducing this blog website
     path('about-this-blog', views.AboutThisBlogView.as_view(), name='about-this-blog'),
+    # Page introducing myself
     path('about-me', views.AboutMeView.as_view(), name='about-me'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
+import collections
 from datetime import date
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic.base import TemplateView
+from django.views.generic.dates import ArchiveIndexView
 
-from .models import Blog, BlogAuthor, BlogComment
+from .models import Blog, BlogAuthor, BlogComment, Tag
 
 
 class Index(generic.ListView):
@@ -17,13 +19,60 @@ class Index(generic.ListView):
     template_name = 'index.html'
 
 
-class BlogListView(generic.ListView):
+class BlogArchiveView(ArchiveIndexView):
     """
-    The generic class-based view for list of all blogs.
+    Archive blogs by dates and tag.
     """
 
     model = Blog
-    paginate_by = 10
+    date_field = "post_date"
+
+    def get_context_data(self, **kwargs):
+        """
+        Add data grouped by date and tag to context.
+        """
+        context = super().get_context_data(**kwargs)
+        object_dict_sorted_by_year = {}
+        for date in context['date_list']:
+            year = date.year
+            object_dict_sorted_by_year[year] = Blog.objects.filter(post_date__year=year)
+        ordered_object_dict_sorted_by_year = collections.OrderedDict(sorted(object_dict_sorted_by_year.items(), reverse=True))
+        context['object_dict_sorted_by_year'] = ordered_object_dict_sorted_by_year
+        
+        object_dict_sorted_by_tag = {}
+        for tag in Tag.objects.all():
+            related_objects = tag.blog_set.all()
+            # related_objects = Blog.objects.filter(tags=tag)
+            if related_objects:
+                object_dict_sorted_by_tag[tag.tag] = related_objects
+        ordered_object_dict_sorted_by_tag = collections.OrderedDict(sorted(object_dict_sorted_by_tag.items()))
+        context['object_dict_sorted_by_tag'] = ordered_object_dict_sorted_by_tag
+
+        return context
+
+
+class BlogArchivedByTagView(generic.ListView):
+    """
+    Archive by tag.
+
+    The URL should be 'archive/<str:tag>/'.
+    """
+
+    model = Blog
+    template_name = "blog/blog_archive_by_tag.html"
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, tag=self.kwargs['tag'])
+        blog_with_tag = Blog.objects.filter(tags=tag)
+        return blog_with_tag
+
+    def get_context_data(self, **kwargs):
+        """
+        Add the tag name to context.
+        """
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.kwargs['tag']
+        return context
 
 
 class BlogDetailView(generic.DetailView):
