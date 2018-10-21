056db99c69ca5ba52ad67d2a1d3cb481b43cca1c
dengmingcong <mc.deng@outlook.com>
2018-10-21 12:09:05 +0800

    change settings.py for deploying

--- pensieve/settings.py
+++ pensieve/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3y^8h=pb@*!q=6p%1c_ezem@@kru9yi*sy(de$@aal@aauo7ko'
+# SECRET_KEY = '3y^8h=pb@*!q=6p%1c_ezem@@kru9yi*sy(de$@aal@aauo7ko'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3y^8h=pb@*!q=6p%1c_ezem@@kru9yi*sy(de$@aal@aauo7ko')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG =bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
