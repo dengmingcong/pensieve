f613d5cf62dd636910075f6caef0c80a2eb0f955
dengmingcong <mc.deng@outlook.com>
2018-12-09 20:17:50 +0800

    change debug log path

--- pensieve/settings.py
+++ pensieve/settings.py
@@ -132,7 +132,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'debug.log'),
+            'filename': '/var/log/pensieve.log'
         },
     },
     'loggers': {
