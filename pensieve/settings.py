37ffef999e227bee85945116ed1301b31afdffc4
dengmingcong <mc.deng@outlook.com>
2020-03-07 13:44:40 +0800

    Add a space after '='.

--- pensieve/settings.py
+++ pensieve/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3y^8h=pb@*!q=6p%1c_ezem@@kru9y
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG =bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
