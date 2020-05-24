d852d660d75613b7602e325d64254fe429b1a1b7
dengmingcong <mc.deng@outlook.com>
2020-05-24 20:42:12 +0800

    Import requirejs to support modules.

--- blog/static/blog/javascript/active-tab.js
+++ blog/static/blog/javascript/active-tab.js
@@ -1,96 +1,105 @@
 "use strict";
 
-const activeMarker = "selected";
-const hiddenMarker = "hidden";
-const postUrl = "/blog/preview-markdown";
+require(["../codemirror/lib/codemirror", "../codemirror/mode/markdown/markdown"], function (CodeMirror) {
+  let cm = CodeMirror.fromTextArea(document.querySelector('textarea'), {
+        lineNumbers: true,
+        mode: "markdown",
+        scrollbarStyle: 'native',
+        lineWrapping: true
+    })
 
-let fileHeader = document.querySelector(".file-header");
-let editTab = fileHeader.querySelector("[data-tab='show-code']");
-let previewTab = fileHeader.querySelector("[data-tab='preview']");
-let editPanel = document.querySelector(".commit-create");
-let loadingPanel = document.querySelector(".loading-preview-msg");
-let previewPanel = document.querySelector(".commit-preview");
+  const activeMarker = "selected";
+  const hiddenMarker = "hidden";
+  const postUrl = "/blog/preview-markdown";
 
-/**
- * Returns the value of specified cookie.
- * @param name
- * @returns {null}
- */
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    let cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      let cookie = cookies[i].trim();
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
+  let fileHeader = document.querySelector(".file-header");
+  let editTab = fileHeader.querySelector("[data-tab='show-code']");
+  let previewTab = fileHeader.querySelector("[data-tab='preview']");
+  let editPanel = document.querySelector(".commit-create");
+  let loadingPanel = document.querySelector(".loading-preview-msg");
+  let previewPanel = document.querySelector(".commit-preview");
+
+  /**
+   * Returns the value of specified cookie.
+   * @param name
+   * @returns {null}
+   */
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      let cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
       }
+    }
+
+    return cookieValue;
+  }
+
+  const dispatchTabs = function dispatchToCodeOrPreview(event) {
+    if (event.target === editTab) {
+      codeTabClicked();
+    } else if (event.target === previewTab) {
+      previewTabClicked();
     }
   }
 
-  return cookieValue;
-}
+  const codeTabClicked = function handlerWhenCodeTabWasClicked() {
+    if (!editTab.classList.contains(activeMarker)) {
 
-const dispatchTabs = function dispatchToCodeOrPreview(event) {
-  if (event.target === editTab) {
-    codeTabClicked();
-  } else if (event.target === previewTab) {
-    previewTabClicked();
+      // Highlight edit tab.
+      editTab.classList.add(activeMarker);
+
+      // Un-highlight preview tab.
+      previewTab.classList.remove(activeMarker);
+
+      // Display editor panel.
+      editPanel.classList.remove(hiddenMarker);
+
+      // Hide preview panel.
+      previewPanel.classList.add(hiddenMarker);
+    }
   }
-}
 
-const codeTabClicked = function handlerWhenCodeTabWasClicked() {
-  if (!editTab.classList.contains(activeMarker)) {
+  const previewTabClicked = function handlerWhenPreviewTabWasClicked() {
+    if (!previewPanel.classList.contains(activeMarker)) {
 
-    // Highlight edit tab.
-    editTab.classList.add(activeMarker);
+      // Highlight preview tab.
+      previewTab.classList.add(activeMarker);
 
-    // Un-highlight preview tab.
-    previewTab.classList.remove(activeMarker);
+      // Un-highlight edit tab.
+      editTab.classList.remove(activeMarker);
 
-    // Display editor panel.
-    editPanel.classList.remove(hiddenMarker);
+      // Hide editor panel.
+      editPanel.classList.add(hiddenMarker);
 
-    // Hide preview panel.
-    previewPanel.classList.add(hiddenMarker);
+      // Hide preview panel.
+      previewPanel.classList.add(hiddenMarker);
+
+      // Display loading panel.
+      loadingPanel.classList.remove(hiddenMarker);
+
+      let xhr = new XMLHttpRequest();
+      let data = new FormData();
+      let csrftoken = getCookie('csrftoken');
+      data.append("origin-markdown", cm.getValue())
+      xhr.addEventListener('load', displayPreview);
+      xhr.open("post", postUrl);
+      xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      xhr.send(data);
+    }
   }
-}
 
-const previewTabClicked = function handlerWhenPreviewTabWasClicked() {
-  if (!previewPanel.classList.contains(activeMarker)) {
-
-    // Highlight preview tab.
-    previewTab.classList.add(activeMarker);
-
-    // Un-highlight edit tab.
-    editTab.classList.remove(activeMarker);
-
-    // Hide editor panel.
-    editPanel.classList.add(hiddenMarker);
-
-    // Hide preview panel.
-    previewPanel.classList.add(hiddenMarker);
-
-    // Display loading panel.
-    loadingPanel.classList.remove(hiddenMarker);
-
-    let xhr = new XMLHttpRequest();
-    let data = new FormData();
-    let csrftoken = getCookie('csrftoken');
-    data.append("origin-markdown", "### This is test text.")
-    xhr.addEventListener('load', displayPreview);
-    xhr.open("post", postUrl);
-    xhr.setRequestHeader("X-CSRFToken", csrftoken);
-    xhr.send(data);
+  const displayPreview = function displayPreviewWhenMarkdownTransformed(event) {
+    loadingPanel.classList.add(hiddenMarker);
+    previewPanel.innerHTML = JSON.parse(event.target.responseText)["html"];
+    previewPanel.classList.remove(hiddenMarker);
   }
-}
 
-const displayPreview = function displayPreviewWhenMarkdownTransformed(event) {
-  loadingPanel.classList.add(hiddenMarker);
-  previewPanel.innerHTML = JSON.parse(event.target.responseText)["html"];
-  previewPanel.classList.remove(hiddenMarker);
-}
-
-fileHeader.addEventListener("click", dispatchTabs, false);
+  fileHeader.addEventListener("click", dispatchTabs, false);
+})
